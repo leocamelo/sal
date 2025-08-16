90379b541dbc0d685943679ca0259df28647727b
leocamelo <leonardocamelo.nave@gmail.com>
2025-08-16 12:21:06 -0300

    add keyword and map literal

--- sal.hs
+++ sal.hs
@@ -1,6 +1,6 @@
 import Data.Char (isAlphaNum, isDigit, isSpace)
 import Data.Fixed (mod')
-import Data.List (findIndex)
+import Data.List (findIndex, intercalate, partition)
 
 import Data.Map qualified as Map
 import Data.Set qualified as Set
@@ -15,23 +15,25 @@ import System.Environment (getArgs)
 data SalVal
   = SalNil
   | SalBool Bool
+  | SalKeyword SalKwI
   | SalNumber Double
   | SalString String
   | SalList [SalVal]
-  | SalKeyword SalKwI
+  | SalMap (Map.Map SalVal SalVal)
   | SalMacro SalMcI
   | SalFunction SalFnI
 
 instance Show SalVal where
   show :: SalVal -> String
   show SalNil = "nil"
-  show (SalBool bool) = show bool
-  show (SalNumber number) = show number
-  show (SalString string) = string
-  show (SalList list) = "[" ++ unwords (map show list) ++ "]"
-  show (SalKeyword keyword) = ':' : keyword
-  show (SalMacro _macro) = "<macro>"
-  show (SalFunction _function) = "<function>"
+  show (SalBool a) = show a
+  show (SalKeyword a) = ':' : a
+  show (SalNumber a) = show a
+  show (SalString a) = a
+  show (SalList a) = "[" ++ unwords (map show a) ++ "]"
+  show (SalMap a) = "{" ++ intercalate ", " (map (\(k, v) -> show k ++ " " ++ show v) $ Map.assocs a) ++ "}"
+  show (SalMacro _) = "<macro>"
+  show (SalFunction _) = "<function>"
 
 instance Eq SalVal where
   (==) :: SalVal -> SalVal -> Bool
@@ -40,18 +42,30 @@ instance Eq SalVal where
   SalNumber a == SalNumber b = a == b
   SalString a == SalString b = a == b
   SalList a == SalList b = a == b
+  SalMap a == SalMap b = a == b
   SalKeyword a == SalKeyword b = a == b
   _ == _ = False
+
+instance Ord SalVal where
+  compare :: SalVal -> SalVal -> Ordering
+  SalBool a `compare` SalBool b = a `compare` b
+  SalNumber a `compare` SalNumber b = a `compare` b
+  SalString a `compare` SalString b = a `compare` b
+  SalList a `compare` SalList b = a `compare` b
+  SalMap a `compare` SalMap b = a `compare` b
+  SalKeyword a `compare` SalKeyword b = a `compare` b
+  _ `compare` _ = EQ
 
 data SalAst
   = SalLit SalVal
   | SalLitList [SalAst]
+  | SalLitMap [SalAst]
   | SalExp (SalKwI, [SalAst])
   | SalRef SalKwI
 
 type SalKwI = String
-type SalFnI = [SalVal] -> IO SalVal
 type SalMcI = SalEnv -> [SalAst] -> IO (SalEnv, [SalAst])
+type SalFnI = [SalVal] -> IO SalVal
 
 type SalEnv = Map.Map SalKwI SalVal
 
@@ -95,17 +109,23 @@ parse = fst . parse' [] ""
 
 parse' :: [SalAst] -> String -> [String] -> ([SalAst], [String])
 parse' acc "" [] = (reverse acc, [])
-parse' acc stp ("nil" : tok) = parse' (SalLit SalNil : acc) stp tok
-parse' acc stp ("true" : tok) = parse' (SalLit (SalBool True) : acc) stp tok
-parse' acc stp ("false" : tok) = parse' (SalLit (SalBool False) : acc) stp tok
-parse' acc stp (('"' : str) : tok) = parse' (SalLit (SalString (init str)) : acc) stp tok
-parse' acc stp ("(" : kw : tok) = let (ast, tok') = parse' [] ")" tok in parse' (SalExp (kw, ast) : acc) stp tok'
-parse' acc stp ("[" : tok) = let (ast, tok') = parse' [] "]" tok in parse' (SalLitList ast : acc) stp tok'
-parse' acc stp (t : tok)
-  | t == stp = (reverse acc, tok)
-  | parse'isNumber t = parse' (SalLit (SalNumber (read t)) : acc) stp tok
-  | parse'isKeyword t = parse' (SalRef t : acc) stp tok
-  | otherwise = error "ParseError: Syntax error"
+parse' acc bp [] = error "Unexpected end of input"
+parse' acc bp ("nil" : tok) = parse' (SalLit SalNil : acc) bp tok
+parse' acc bp ("true" : tok) = parse' (SalLit (SalBool True) : acc) bp tok
+parse' acc bp ("false" : tok) = parse' (SalLit (SalBool False) : acc) bp tok
+parse' acc bp ((':' : kw) : tok) = parse' (SalLit (SalKeyword kw) : acc) bp tok
+parse' acc bp (('"' : str) : tok) = parse' (SalLit (SalString (init str)) : acc) bp tok
+parse' acc bp ("[" : tok) = let (ast, tok') = parse' [] "]" tok in parse' (SalLitList ast : acc) bp tok'
+parse' acc bp ("{" : tok) = let (ast, tok') = parse' [] "}" tok in parse' (SalLitMap (parse'map ast) : acc) bp tok'
+parse' acc bp ("(" : kw : tok) = let (ast, tok') = parse' [] ")" tok in parse' (SalExp (kw, ast) : acc) bp tok'
+parse' acc bp (t : tok)
+  | t == bp = (reverse acc, tok)
+  | parse'isNumber t = parse' (SalLit (SalNumber (read t)) : acc) bp tok
+  | parse'isKeyword t = parse' (SalRef t : acc) bp tok
+  | otherwise = error "Syntax error"
+
+parse'map :: [SalAst] -> [SalAst]
+parse'map ast = if even (length ast) then ast else error "Map literal must contain an even number of forms"
 
 parse'isNumber :: String -> Bool
 parse'isNumber (h : t) = isDigitNeg h && all isDigitDot t && dotCount t <= 1
@@ -130,20 +150,26 @@ run = run' []
 run' :: [SalVal] -> SalEnv -> [SalAst] -> IO (SalEnv, [SalVal])
 run' acc env [] = pure (env, reverse acc)
 run' acc env (SalLit val : ast) = run' (val : acc) env ast
-run' acc env (SalLitList ast' : ast) = run env ast' >>= \(env', list) -> run' (SalList list : acc) env' ast
-run' acc env (SalExp (kw, ast') : ast) = run'exp env kw ast' >>= \(env', val') -> run' (val' ++ acc) env' ast
+run' acc env (SalLitList ast' : ast) = run env ast' >>= \(env', vals) -> run' (SalList vals : acc) env' ast
+run' acc env (SalLitMap ast' : ast) = run env ast' >>= \(env', vals) -> run' (SalMap (run'map vals) : acc) env' ast
+run' acc env (SalExp (kw, ast') : ast) = run'exp env kw ast' >>= \(env', vals) -> run' (vals ++ acc) env' ast
 run' acc env (SalRef kw : ast) = run' (run'ref env kw : acc) env ast
+
+run'map :: [SalVal] -> Map.Map SalVal SalVal
+run'map vals = Map.fromList $ zip (map snd keys) (map snd values)
+ where
+  (keys, values) = partition (even . fst) $ zip [0 ..] vals
 
 run'exp :: SalEnv -> SalKwI -> [SalAst] -> IO (SalEnv, [SalVal])
 run'exp env kw ast = case Map.lookup kw env of
   Just (SalMacro mc) -> mc env ast >>= uncurry run
   Just (SalFunction fn) -> run env ast >>= fn . snd >>= \val -> pure (env, [val])
-  _ -> error "RuntimeError: Is not a function or macro"
+  _ -> error ("`" ++ kw ++ "` is not a function")
 
 run'ref :: SalEnv -> SalKwI -> SalVal
 run'ref env kw = case Map.lookup kw env of
   Just val -> val
-  Nothing -> error "RuntimeError: Undefined variable"
+  Nothing -> error ("Undefined variable `" ++ kw ++ "`")
 
 --------------------------------------------------------------------------------
 -- PRELUDE
@@ -158,13 +184,16 @@ sal'castBool _ = True
 
 sal'castNumber :: SalVal -> Double
 sal'castNumber (SalNumber number) = number
-sal'castNumber _ = error "RuntimeError: Not a number"
+sal'castNumber val = error ("`" ++ show val ++ "` is not a number")
 
 sal'bool :: Bool -> IO SalVal
 sal'bool = pure . SalBool
 
 sal'number :: Double -> IO SalVal
 sal'number = pure . SalNumber
+
+sal'string :: String -> IO SalVal
+sal'string = pure . SalString
 
 sal'keyword :: SalKwI -> IO SalVal
 sal'keyword = pure . SalKeyword
@@ -173,28 +202,33 @@ sal'keyword = pure . SalKeyword
 
 sal'if :: SalMcI
 sal'if env [cond, true] = sal'if env [cond, true, SalLit SalNil]
-sal'if env [cond, true, false] = run env [cond] >>= \(env', [val]) -> pure (env', if sal'castBool val then [true] else [false])
+sal'if env [cond, true, false] =
+  run env [cond] >>= \(env', [val]) -> pure (env', if sal'castBool val then [true] else [false])
 
 sal'fn :: SalMcI
 sal'fn env ast@(SalLitList _ : (_ : _)) = sal'fn env (SalRef "" : ast)
-sal'fn env (SalRef kw : SalLitList args'keys : ast@(_ : _)) = pure (env, [SalLit fn])
+sal'fn env (SalRef kw : SalLitList args'keys : ast@(_ : _)) = pure (env, [SalLit fn'])
  where
-  fn = SalFunction fn'
-  fn' args'values = last . snd <$> run (fn'env args'values) ast
+  fn' = SalFunction fn'cls
+  fn'cls args'values = last . snd <$> run (fn'env args'values) ast
   fn'env args'values = foldl env'ref env' $ zip args'keys args'values
-  env' = Map.insert "recur" fn $ if kw == "" then env else Map.insert kw fn env
+  env' = Map.insert "recur" fn' $ if kw == "" then env else Map.insert kw fn' env
   env'ref e (SalRef k, v) = Map.insert k v e
 
 sal'def :: SalMcI
-sal'def env [SalRef kw, ast] = run env [ast] >>= \(env', [val]) -> pure (Map.insert kw val env', [SalLit (SalKeyword kw)])
+sal'def env [SalRef kw, ast] =
+  run env [ast] >>= \(env', [val]) -> pure (Map.insert kw val env', [SalLit (SalKeyword kw)])
 
 sal'defn :: SalMcI
-sal'defn env ast@(ref@(SalRef _) : (args@(SalLitList _) : (_ : _))) = sal'fn env ast >>= \(env', ast') -> sal'def env' (ref : ast')
+sal'defn env ast@(ref@(SalRef _) : (args@(SalLitList _) : (_ : _))) =
+  sal'fn env ast >>= \(env', ast') -> sal'def env' (ref : ast')
 
 -- Functions
 
 sal'println :: SalFnI
-sal'println values = putStrLn (unwords $ map show values) >> pure SalNil
+sal'println values = do
+  putStrLn (unwords $ map show values)
+  pure SalNil
 
 sal'type :: SalFnI
 sal'type [val] = sal'keyword $ typeOf val
@@ -217,6 +251,8 @@ sal'mul = sal'number . product . map sal'castNumber
 sal'div :: SalFnI
 sal'div = sal'number . foldl1 (/) . map sal'castNumber
 
+sal'abs :: SalFnI
+sal'abs [SalNumber a] = sal'number $ abs a
 sal'mod :: SalFnI
 sal'mod [SalNumber a, SalNumber b] = sal'number $ mod' a b
 
@@ -230,9 +266,6 @@ sal'max = sal'number . maximum . map sal'castNumber
 sal'min :: SalFnI
 sal'min = sal'number . minimum . map sal'castNumber
 
-sal'eq :: SalFnI
-sal'eq [a, b] = sal'bool $ a == b
-
 sal'gt :: SalFnI
 sal'gt [SalNumber a, SalNumber b] = sal'bool $ a > b
 sal'gte :: SalFnI
@@ -243,6 +276,8 @@ sal'lt [SalNumber a, SalNumber b] = sal'bool $ a < b
 sal'lte :: SalFnI
 sal'lte [SalNumber a, SalNumber b] = sal'bool $ a <= b
 
+sal'eq :: SalFnI
+sal'eq [a, b] = sal'bool $ a == b
 sal'or :: SalFnI
 sal'or = sal'bool . any sal'castBool
 sal'and :: SalFnI
@@ -254,7 +289,10 @@ sal'noteq :: SalFnI
 sal'noteq [a, b] = sal'eq [a, b] >>= \c -> sal'not [c]
 
 sal'nil :: SalFnI
-sal'nil [val] = sal'bool $ val == SalNil
+sal'nil [value] = sal'bool $ value == SalNil
+
+sal'str :: SalFnI
+sal'str = sal'string . concatMap show
 
 -- Export
 
@@ -271,16 +309,17 @@ prelude =
     , ("-", SalFunction sal'sub)
     , ("*", SalFunction sal'mul)
     , ("/", SalFunction sal'div)
+    , ("abs", SalFunction sal'abs)
     , ("mod", SalFunction sal'mod)
     , ("inc", SalFunction sal'inc)
     , ("dec", SalFunction sal'dec)
     , ("max", SalFunction sal'max)
     , ("min", SalFunction sal'min)
-    , ("=", SalFunction sal'eq)
     , (">", SalFunction sal'gt)
     , (">=", SalFunction sal'gte)
     , ("<", SalFunction sal'lt)
     , ("<=", SalFunction sal'lte)
+    , ("=", SalFunction sal'eq)
     , ("or", SalFunction sal'or)
     , ("and", SalFunction sal'and)
     , ("not", SalFunction sal'not)
@@ -298,9 +337,22 @@ eval code = snd <$> eval' prelude code
 eval' :: SalEnv -> String -> IO (SalEnv, [SalVal])
 eval' env = run env . parse . tokenize
 
+repl :: IO ()
+repl = loop prelude 1
+ where
+  loop env line = do
+    putStr $ "sal:" ++ show line ++ "> "
+    (env', vals) <- getLine >>= eval' env
+    mapM_ print vals
+    loop env' $ succ line
+
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [file] -> readFile file >>= void . eval
-    [] -> putStrLn "REPL AQUI"
+    [file] -> do
+      code <- readFile file
+      void $ eval code
+    [] -> do
+      putStrLn "Sal's Another Lisp"
+      repl
